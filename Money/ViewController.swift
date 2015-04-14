83acc5a412210f6d04688ef7a1e58904e5e21a0a
rictyran <rictyran@gmail.com>
2015-04-14 11:23:05 -0400

    Swift app that includes iAd advertising banner
    final

---
+++ Money/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Money
+//
+//  Created by Richard Tyran on 3/4/15.
+//  Copyright (c) 2015 Richard Tyran. All rights reserved.
+//
+
+import UIKit
+import iAd
+
+class ViewController: UIViewController {
+    @IBOutlet var adBanner: ADBannerView!
+
+    
+    // This code creates a banner that changes size depending on device/orientation:
+//    func adBanner() -> ADBannerView {
+//        
+//
+    
+//    var adBanner = ADBannerView(adType: ADAdType.Banner)
+//    
+//    adBanner.frame = CGRectMake(0,0, UIScreen.mainScreen().bounds.size.width, 66)
+//    
+//    return adBanner
+//}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
